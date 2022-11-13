9f882a76f72c387aad22e52dbe5600b9b7727b9c
@mymactive <mymactive@gmail.com>
2022-11-13 21:39:42 +0900

    feat: Chapter5 Modeling Unknown Types and Modeling with Choice Types

--- src/domain/order-taking/Types.fs
+++ src/domain/order-taking/Types.fs
@@ -1,3 +1,28 @@
 type WidgetCode = string
+type GizimoCode = string
 type UnitQuantity = int
 type KilogramQuantity = decimal
+
+type ProductCode =
+    | WidgetCode
+    | GizimoCode
+
+type OrderQuantity =
+    | UnitQuantity
+    | KilogramQuantity
+
+
+type Undefined = exn
+
+type CustomerInf = Undefined
+type ShippingAddress = Undefined
+type BillingAddress = Undefined
+type OrderLine = Undefined
+type BillingAmount = Undefined
+
+type Order =
+    { CustoomerInfo: CustomerInfo
+      ShippingAddress: ShippingAddress
+      BillingAddress: BillingAddress
+      OrderLines: OrderLine list
+      AmountToBill: BillingAmount }
