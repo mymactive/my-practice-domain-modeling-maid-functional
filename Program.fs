53705c522c710e605ffd68b8c30add39f981afcb
Miyazaki Masashi <mymactive@gmail.com>
2022-11-14 23:02:12 +0900

    Chapter 5 up to Immutability and Identity

--- Program.fs
+++ Program.fs
@@ -1,2 +1,9 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
 printfn "Hello from F#"
+
+type WidgetCode = string
+
+let WidgetCode1 = WidgetCode "W1234"
+let WidgetCode2 = WidgetCode "W1234"
+
+printfn "%b" (WidgetCode1 = WidgetCode2)
--- src/domain/order-taking/Types.fs
+++ src/domain/order-taking/Types.fs
@@ -1,3 +1,5 @@
+namespace OrderTaking.Types
+
 type WidgetCode = string
 type GizimoCode = string
 type UnitQuantity = int
@@ -14,15 +16,56 @@ type OrderQuantity =
 
 type Undefined = exn
 
-type CustomerInf = Undefined
+type CustomerInfo = Undefined
 type ShippingAddress = Undefined
 type BillingAddress = Undefined
-type OrderLine = Undefined
 type BillingAmount = Undefined
 
-type Order =
-    { CustoomerInfo: CustomerInfo
-      ShippingAddress: ShippingAddress
-      BillingAddress: BillingAddress
-      OrderLines: OrderLine list
-      AmountToBill: BillingAmount }
+type OrderId = Undefined
+type ProductId = Undefined
+
+[<NoEquality; NoComparison>]
+type OrderLine = {
+    OrderId: OrderId
+    ProductId: ProductId
+    Qty: int
+}
+with
+    member this.Key = 
+        (this.OrderId, this.ProductId)
+
+type Order ={ 
+    CustoomerInfo: CustomerInfo
+    ShippingAddress: ShippingAddress
+    BillingAddress: BillingAddress
+    OrderLines: OrderLine list
+    AmountToBill: BillingAmount }
+
+type QuoteForm = Undefined
+type OrderForm = Undefined
+
+type EnvelopeContents = string
+type CateforizedMail = Quote of QuoteForm | Order of OrderForm
+
+type UnvalidatedOrder = Undefined
+type ValidatedOrder = Undefined
+
+type ValidationResponse<'a> = Async<Result<'a, ValidationError>>
+and ValidationError = {
+    FieldName: string
+    ErrorDescription: string
+}
+
+type ValidateOrder = UnvalidatedOrder -> ValidationResponse<ValidatedOrder>
+
+type ContactId = string
+type PhoneNumber = string
+type EmailAddress = string
+
+[<NoEquality; NoComparison>]
+type Contact = {
+    ContactId: ContactId
+    PhoneNumber: PhoneNumber
+    EmailAddress: EmailAddress
+}
+
